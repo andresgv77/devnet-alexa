a4f8a803406ea17be8172448aa3609366d814c31
andresgv77 <57276797+andresgv77@users.noreply.github.com>
2019-11-01 21:21:47 -0500

    Update ucsm_operations.py

--- ucsm_operations.py
+++ ucsm_operations.py
@@ -5,6 +5,9 @@ Purpose:
 Author:
     John McDonough (jomcdono@cisco.com)
     Cisco Systems, Inc.
+CoAuthor
+    Andrés Guevara y Matheo López
+    UDLA
 """
 
 import os
@@ -16,7 +19,7 @@ ucsmhost = os.environ['UCSMHOST']
 handle = None
 status = {}
 
-# Add a VLAN to UCS Manager
+# Agregar una VLAN al administrador de UCS
 def add_ucs_vlan(vlan_id):
 
     vlan_id_num = int(vlan_id)
@@ -24,14 +27,14 @@ def add_ucs_vlan(vlan_id):
     from ucsmsdk.mometa.fabric.FabricVlan import FabricVlan
     
     if vlan_id == "1":
-        message = ("For the requested UCS Manager V Lan add operation, " +
-            "V Lan 1 can be given additional names, however, this skill does not allow for that procedure.")
+        message = ("Para la operación de agregar Lan de UCS Manager V solicitada, " +
+            "V Lan 1 puede recibir nombres adicionales, sin embargo, esta habilidad no permite ese procedimiento.")
         return message
     elif ((vlan_id_num <= 1) or
         (vlan_id_num >= 4030 and vlan_id_num <= 4048) or
         (vlan_id_num > 4093)):
-        message = ("For the requested UCS Manager V Lan add operation, " + 
-            "the provided V Lan I D " + vlan_id + ", is not allowed.")
+        message = ("Para la operación de agregar Lan de UCS Manager V solicitada, " + 
+            "el ID de V Lan proporcionado " + vlan_id + ", no está permitido.")
         return message
 
     ucsm_login()
@@ -39,8 +42,8 @@ def add_ucs_vlan(vlan_id):
     if status['login'] == "success":
         response = handle.query_dn("fabric/lan/net-vlan" + vlan_id)
     else:
-        message = ("there was an error connecting to the UCS Manager, " +
-            "please check the access credentials or IP address")
+        message = ("hubo un error al conectarse al Administrador UCS, " +
+            "compruebe las credenciales de acceso o la dirección IP")
         return message
 
     if not response:
@@ -55,22 +58,22 @@ def add_ucs_vlan(vlan_id):
         response = handle.query_dn("fabric/lan/net-vlan" + vlan_id)
 
         if response and response.name == "vlan" + vlan_id:
-            message = "V Lan " + vlan_id + " has been added to UCS Manager."
+            message = "V Lan " + vlan_id + " se ha agregado al Administrador de UCS."
         else:
-            message = "V Lan " + vlan_id + " was not added to UCS Manager."
+            message = "V Lan " + vlan_id + " no se agregó al Administrador de UCS."
     else:
-        message = "V Lan " + vlan_id + " already exists on UCS Manager."
+        message = "V Lan " + vlan_id + " ya existe en UCS Manager."
     
     ucsm_logout()
 
-    return "For the requested UCS Manager V Lan add operation, " + message
+    return "Operación de agregación de UCS Manager V Lan solicitada, " + message
 
 # Remove a VLAN from UCS Manager
 def remove_ucs_vlan(vlan_id):
     
     if vlan_id == "1":
-        message = ("For the requested UCS Manager V Lan remove operation, " + 
-            "this skill does not support the removal of V Lan 1.")
+        message = (" Operación de eliminación de UCS Manager V Lan solicitada, " + 
+            "Esta habilidad no admite la eliminación de V Lan 1.")
         return message
 
     ucsm_login()
@@ -78,8 +81,8 @@ def remove_ucs_vlan(vlan_id):
     if status['login'] == "success":
         response = handle.query_dn("fabric/lan/net-vlan" + vlan_id)
     else:
-        message = ("there was an error connecting to the UCS Manager, " +
-            "please check the access credentials or IP address")
+        message = ("hubo un error al conectarse al Administrador UCS, " +
+            "compruebe las credenciales de acceso o la dirección IP")
         return message
 
     if response and response.name == "vlan" + vlan_id:
@@ -90,15 +93,15 @@ def remove_ucs_vlan(vlan_id):
         response = handle.query_dn("fabric/lan/net-vlan" + vlan_id)
 
         if not response:
-            message = "V Lan " + vlan_id + " has been removed from UCS Manager."
+            message = "V Lan " + vlan_id + " se ha eliminado del Administrador de UCS."
         else:
-            message = "V Lan " + vlan_id + " was not removed from UCS Manager."
+            message = "V Lan " + vlan_id + " no se eliminó del Administrador de UCS ."
     else:
-        message = "V Lan " + vlan_id + " does not exist on UCS Manager."
+        message = "V Lan " + vlan_id + " no existe en UCS Manager."
     
     ucsm_logout()
 
-    return "For the requested UCS Manager V Lan remove operation, " + message
+    return "Operación de eliminación de UCS Manager V Lan solicitada, " + message
 
 # Retrieve fault counts for, critical, major, minor and warning faults from UCS Manager
 def get_ucs_faults():
@@ -108,8 +111,8 @@ def get_ucs_faults():
     if status['login'] == "success":
         response = handle.query_classid("FaultInst")
     else:
-        message = ("there was an error connecting to the UCS Manager, " +
-            "please check the access credentials or IP address")
+        message = ("hubo un error al conectarse al Administrador UCS, " +
+            "compruebe las credenciales de acceso o la dirección IP ")
         return message
     
     ucsm_logout()
@@ -120,24 +123,24 @@ def get_ucs_faults():
     sev_warning  = 0
 
     for fault in response:
-        if fault.severity == 'critical':
+        if fault.severity == 'critico':
             sev_critical += 1
-        elif fault.severity == 'major':
+        elif fault.severity == 'mayor':
             sev_major += 1
-        elif fault.severity == 'minor':
+        elif fault.severity == 'menor':
             sev_minor += 1
-        elif fault.severity == 'warning':
+        elif fault.severity == 'advertencia':
             sev_warning += 1
  
-    message = ("For the requested UCS Manager fault retrieval operation, there are " +
-        str(sev_critical) + " critical faults, " +
-        str(sev_major) + " major faults, " +
-        str(sev_minor) + " minor faults, and " +
-        str(sev_warning) + " warnings")
+    message = ("Para la operación de recuperación de fallas del Administrador UCS solicitada, hay " +
+        str(sev_critical) + " fallas criticas, " +
+        str(sev_major) + " fallas mayores, " +
+        str(sev_minor) + " fallas menores, y " +
+        str(sev_warning) + " advertencias")
 
     return message
 
-# Create and Associate a Service Profile to an available server
+# Crear y asociar un perfil de servicio a un servidor disponible
 def set_ucs_server():
 
     from ucsmsdk.mometa.ls.LsServer import LsServer
@@ -149,54 +152,54 @@ def set_ucs_server():
     ucsm_login()
 
     if status['login'] != "success":
-        message = ("there was an error connecting to the UCS Manager, " +
-            "please check the access credentials or IP address")
+        message = ("hubo un error al conectarse al Administrador UCS, " +
+            "compruebe las credenciales de acceso o la dirección IP")
         return message
     
-    # Create Org DevNet
+    # Crear Org DevNet
     mo_org = OrgOrg(parent_mo_or_dn="org-root", name="DevNet")
     handle.add_mo(mo_org, modify_present=True)
     handle.commit()
 
-    # Create MacPool
+    # Crear MacPool
     mac_pool_default = handle.query_dn("org-root/mac-pool-default")
     mo_mac_pool_block = MacpoolBlock(parent_mo_or_dn=mac_pool_default,r_from="00:25:B5:00:00:AA",to="00:25:B5:00:00:D9")
     handle.add_mo(mo_mac_pool_block, modify_present=True)
     handle.commit()
 
-    # Add/Update Service Profile Template
+    # Agregar / actualizar plantilla de perfil de servicio
     mo_sp_template = LsServer(parent_mo_or_dn="org-root/org-DevNet", type="initial-template", name="DevNet_Skill_Template")
     handle.add_mo(mo_sp_template, modify_present=True)
     handle.commit()
 
-    # Retrive the MO for the created/updated Service Profile template
+    # Reecupere el MO para la plantilla de perfil de servicio creada / actualizada
     filter_exp = '(name,"DevNet_Skill_Template")'
     mo_sp_templ = handle.query_classid("lsServer",filter_str=filter_exp)
     
-    # Retrive MOs for any existing Service Profiles
+    # Recuperar MOs para cualquier perfil de servicio existente
     filter_exp = '(name,"DevNet_Skill_*", type="re") and (type,"instance")'
     mo_sp_instances = handle.query_classid("lsServer",filter_str=filter_exp)
     
-    # Find the highest suffix for existing Service Profiles
+    # Encuentra el sufijo más alto para los perfiles de servicio existentes
     if len(mo_sp_instances) >= 1:
         sp_suffixes = [int(sp_instance.name[sp_instance.name.rindex('_')+1:]) for sp_instance in mo_sp_instances]
         num_sp_instances = max(sp_suffixes) + 1
     else:
         num_sp_instances =1
 
-    # Create the next Service Profile name
+    # Crear el siguiente nombre de perfil de servicio
     if num_sp_instances <= 9:
         service_profile_name = "DevNet_Skill_Server_0" + str(num_sp_instances)
     else:
         service_profile_name = "DevNet_Skill_Server_" + str(num_sp_instances)
 
-    # Find an available compute blade
+    # Encuentra una hoja de cómputo disponible
     response = handle.query_classid("computeBlade")
     for blade in response:
         if blade.admin_state == 'in-service' and blade.availability == 'available':
             break
 
-    # Create the Service Profile
+    # Crear el perfil de servicio
     mo_sp = LsServer(parent_mo_or_dn="org-root/org-DevNet", src_templ_name="DevNet_Skill_Template", name=service_profile_name)
     mo_sp_templ_ls_binding = LsBinding(parent_mo_or_dn=mo_sp, pn_dn=blade.dn)
     handle.add_mo(mo_sp, modify_present=True)
@@ -204,15 +207,15 @@ def set_ucs_server():
 
     ucsm_logout()
 
-    message = ("For the requested UCS Manager Server Provisioning operation," +
-        " server, " + blade.slot_id + ", " + 
-        " in chassis, " + blade.chassis_id + ", " + 
-        " has been provisioned with service profile, " + service_profile_name.replace('_',' ') + "," +
-        " in the Dev Net Organization.")
+    message = ("Para la operación de aprovisionamiento del servidor UCS Manager solicitada," +
+        " servidor, " + blade.slot_id + ", " + 
+        " en el chasis, " + blade.chassis_id + ", " + 
+        " se ha aprovisionado con el perfil de servicio, " + service_profile_name.replace('_',' ') + "," +
+        " en la organización Dev Net.")
     
     return message
 
-# Retrieve fault counts for, critical, major, minor and warning faults from UCS Manager
+# Recuperar recuentos de fallas para fallas críticas, mayores, menores y de advertencia del Administrador UCS
 def reset_ucs_skill():
 
     ucsm_login()
@@ -220,17 +223,17 @@ def reset_ucs_skill():
     if status['login'] == "success":
         pass
     else:
-        message = ("there was an error connecting to the UCS Manager, " +
-            "please check the access credentials or IP address")
+        message = ("hubo un error al conectarse al Administrador UCS, " +
+            "compruebe las credenciales de acceso o la dirección IP ")
         return message
     
-    # Remove the DevNet Org
+    # Eliminar la organización DevNet
     mo_org = handle.query_dn("org-root/org-DevNet")
     if mo_org:
         handle.remove_mo(mo_org)
         handle.commit()
     
-    # Remove all VLANs other than VLAN 1
+    # Eliminar todas las VLAN que no sean VLAN 1
     mo_vlans = handle.query_classid("fabricVlan")
     if mo_vlans:
         for vlan in mo_vlans:
@@ -239,7 +242,7 @@ def reset_ucs_skill():
         
         handle.commit()
 
-    # Remove all MAC Pool Blocks from the default MAC Pool
+    # Eliminar todos los bloques de grupo MAC del grupo MAC predeterminado
     mo_mac_pool_blocks = handle.query_classid("macpoolBlock")
     if mo_mac_pool_blocks:
         for block in mo_mac_pool_blocks:
@@ -247,7 +250,7 @@ def reset_ucs_skill():
         
         handle.commit()
 
-    message = "For the requested UCS Manager clean up operation, the UCS Manager has been cleaned."
+    message = "Para la operación de limpieza del Administrador de UCS solicitada, el Administrador de UCS se ha limpiado."
 
     return message
 
