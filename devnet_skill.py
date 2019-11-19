5cf5fa050056f4d7df8839b3e787f976da00eb2d
andresgv77 <57276797+andresgv77@users.noreply.github.com>
2019-11-19 17:44:14 -0500

    Add files via upload

---
+++ devnet_skill.py
@@ -0,0 +1,181 @@
+"""
+devnet_skill.py
+Author:
+    John McDonough (jomcdono@cisco.com)
+    Cisco Systems, Inc.
+CoAuthores:
+Andres Guevara
+Matheo Lopez
+"""
+from __future__ import print_function
+
+
+# Importar las funciones de gestión de UCS
+
+import ucsm_operations
+
+# Ayudantes que construyen todas las respuestas
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+# Funciones que controlan el comportamiento de la habilidad 
+
+def get_welcome_response():
+
+    session_attributes = {}
+    card_title = "Bienvenido"
+    speech_output = "Bienvenido a la Habilidad DevNet Alexa para la Gestión de UCS." \
+                    "Se puede hacer preguntas como: ¿Cuál es el total de fallas?" \
+                    "O se puede decir Añadir V Lan 100. " \
+                    " O puedes decir Aprovisionar un servidor"
+					
+    # Si el usuario no responde al mensaje de bienvenida o dice algo
+    # que no se entiende, se les volverá a preguntar con este texto.
+	
+    reprompt_text = "¿Desea hacer algo o saber algo sobre su sistema UCS?"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def handle_session_end_request():
+    card_title = "Sesion finalizada"
+    speech_output = "Gracias por usar Alexa Skill para la administración de UCS."
+
+     # Establecer esto en verdadero finaliza la sesión y sale de la habilidad.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+# Obtenga las fallas UCS
+
+def get_faults(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    speech_output = ucsm_operations.get_ucs_faults()
+    should_end_session = True
+
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# Agregar a UCS VLAN
+def add_vlan(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    speech_output = ucsm_operations.add_ucs_vlan(intent['slots']['vlan_id']['value'])
+    should_end_session = True
+
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# Eliminar a UCS VLAN
+def remove_vlan(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    speech_output = ucsm_operations.remove_ucs_vlan(intent['slots']['vlan_id']['value'])
+    should_end_session = True
+
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# Crear y asociar un Service Profile a un servidor disponible
+def set_server(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    speech_output = result = ucsm_operations.set_ucs_server()
+    should_end_session = True
+
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+# Eventos
+
+def on_session_started(session_started_request, session):
+  
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+def on_launch(launch_request, session):
+ 
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+def on_intent(intent_request, session):
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "ObtenerFallas":     # Entry point for the GetFaults intent that you created
+        return get_faults(intent, session)
+    elif intent_name == "AgregarVlan":         # Entry point for the AddVlan intent that you created
+        return add_vlan(intent, session)
+    elif intent_name == "EliminarVlan":      # Entry point for the RemoveVlan intent that you may have created :)
+        return remove_vlan(intent, session)
+    elif intent_name == "AprovisionarServidor": # Entry point for the ProvisionServer intent that you created
+        return set_server(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Intento no valido")
+
+
+def on_session_ended(session_ended_request, session):
+  
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+  
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
